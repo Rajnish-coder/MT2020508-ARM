97e3678ff8cca479f72f487029673a37491914f3
Rajnish <70737927+Rajnish-coder@users.noreply.github.com>
2021-09-05 11:54:35 +0530

    Update q1.c

--- assign_one/q1.c
+++ assign_one/q1.c
@@ -11,15 +11,15 @@ int main()
 
     scanf("%d %d %d",&num1,&num2,&num3);
 
-    if(num1>num2 && num1>num3)
+    if(num1>=num2 && num1>=num3)
     {
         ans = num1;
     }
-    if(num2>num1 && num2>num3)
+    if(num2>=num1 && num2>=num3)
     {
         ans = num2;
     }
-    if(num3>num1 && num3>num2)
+    if(num3>=num1 && num3>=num2)
     {
         ans=num3;
     }
